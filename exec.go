753b2103520608c2ed958764bfdb5f5bfbb20698
email <naufal.ziyad@detik.com>
2019-04-19 07:31:43 +0700

    learn exec

---
+++ exec.go
@@ -0,0 +1,17 @@
+package main
+
+import ("fmt"
+"os/exec")
+
+func main(){
+	var output1, _ = exec.Command("1s").Output()
+	fmt.Printf("-> ls\n%s\n", string(output1))
+
+	var output2, _ = exec.Command("pwd").Output()
+	fmt.Printf("-> ls\n%s\n", string(output2))
+
+	var output3, _ = exec.Command("git", "config", "user.name").Output()
+	fmt.Printf("-> git config user.name\n%s\n", string(output3))
+
+	
+}
